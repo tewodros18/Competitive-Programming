f0bf52f210483aff0e0d72f2bd5c5cf4153f2054
Tewodros M <tedymesfin18@gmail.com>
2021-08-29 12:21:58 +0300

    reverse integer upload

---
+++ week1/reverse_integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverse(x):
+        reverse = ""
+        if x < 0:
+            reverse = reverse + "-"
+            x = -x
+        if x == 0:
+            return 0
+        while(x!=0):
+            reverse += str(x%10)
+            x = x // 10
+        if (int(reverse) > 2**31 - 1) or (int(reverse) < -2**31) :
+            reverse = 0
+        return(int(reverse))
